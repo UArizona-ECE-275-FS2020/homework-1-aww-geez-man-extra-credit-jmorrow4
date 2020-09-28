17433838305d0ded11014a5b863496a1daf22c2b
jmorrow4 <70256544+jmorrow4@users.noreply.github.com>
2020-09-28 00:30:20 -0700

    Add files via upload
    ...

--- src/Morty.cpp
+++ src/Morty.cpp
@@ -9,13 +9,68 @@
 
 // Include the Morty header file
 #include "Morty.hpp"
+using namespace std;
+using namespace C137;
+using namespace Z286;
+using namespace interdimensional;
 
 // =========================================================================== // 
 //          Define functions enclosed in Namespace C137 here 
+
+
+
+void C137::Morty(int start, int stop, int step) {
+    cout << "Morty C137 says:" << endl;
+    for (int i = start; i <= stop; i = i + step) {
+        if (i % 15 == 0) { cout << i << ": Aww Geez Man" << endl; }
+        else if (i % 5 == 0) { cout << i << ": Geez" << endl; }
+        else if (i % 3 == 0) { cout << i << ": Aww" << endl; }
+        else cout << i << ": " << endl;
+    }
+}
+void C137::Morty(int start, int stop) {
+   C137::Morty(start, stop, 1);
+}
 // =========================================================================== // 
 
 
 
 // =========================================================================== // 
 //          Define functions enclosed in Namespace Z286 here 
+
+
+void Z286::Morty(int start, int stop, int step) {
+    cout << "Morty Z286 says:" << endl;
+    for (int i = start; i <= stop; i = i + step) {
+        if (i % 15 == 0) { cout << i << ": naM zeeG wwA" << endl; }
+        else if (i % 5 == 0) { cout << i << ": zeeG" << endl; }
+        else if (i % 3 == 0) { cout << i << ": wwA" << endl; }
+        else cout << i << ": " << endl;
+    }
+}
+void Z286::Morty(int start, int stop) {
+    Z286::Morty(start, stop, 1);
+}
+     
+    
+
 // =========================================================================== // 
+
+args interdimensional::parseArgs(int c, char** v) {
+    args variable;
+    variable.start = atoi(*(v + 1));
+    variable.stop = atoi(*(v + 2));
+    if (c == 5) {
+        variable.step = atoi(*(v + 3));
+    }
+    if (*(v + c - 1) == "C137") {
+        variable.dim = C137;
+    }
+    else if (*(v + c - 1) == "Z286") {
+        variable.dim = Z286;
+    }
+    else {
+        variable.dim = UNKNOWN;
+    }
+    
+}
